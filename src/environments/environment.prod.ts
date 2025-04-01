b2cac1ad08d49b7db3523722d5ede82edbc48eda
DanielGamliel2108 <161375143+DanielGamliel2108@users.noreply.github.com>
2025-04-01 20:17:43 +0300

    update files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+        apiKey: "AIzaSyBTIw85VQ7zbQ6qMpTck7caUcxvDgdvLOw",
+        authDomain: "ang-blog-561e8.firebaseapp.com",
+        projectId: "ang-blog-561e8",
+        storageBucket: "ang-blog-561e8.firebasestorage.app",
+        messagingSenderId: "612292226211",
+        appId: "1:612292226211:web:de513fde131b192d05a316"
+      }
+    };
+
